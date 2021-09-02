2833ad1e7f4b5fd87255be51201a963cd51f832a
Sergio Garcia Mondaray <sgmonda@gmail.com>
2021-09-02 20:04:11 +0200

    feat: fetch working with server side caching

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -6,22 +6,22 @@ const ENDPOINT = 'https://6f7smj4fdc.execute-api.us-east-1.amazonaws.com/default
 
 let cache: User[] = [];
 
-(async () => {
-  try {
-    const { data } = await axios.get(ENDPOINT);
-    cache = data;
-  } catch (err) {
-    console.log('ERROR', err);
-  }
-})()
-
 const endpoint: NextApiHandler = async ({ headers }, res) => {
   const { token } = headers;
   if (token !== 'TEST_TOKEN') {
     // Only authorized users can list data
     return res.status(403).json({ error: 'Unauthorized' });
   }
-  const data = cache;
+  let data: User[] = cache;
+  if (!data.length) {
+    try {
+      const resp = await axios.get(ENDPOINT);
+      data = resp.data;
+    } catch (err) {
+      console.log('ERROR', err);
+    }
+    cache = data;
+  }
   res.status(200).json(data);
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,11 @@ import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { selector as authSelector } from '../store/auth';
 import { selector as usersSelector, setListData } from '../store/users';
 import { User } from '../common';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const IndexPage: React.FC = () => {
-
-  const auth = useAppSelector(authSelector)
-
+  const auth = useAppSelector(authSelector);
   return auth.user ? <UsersList /> : <NotAuthError />;
 };
 
@@ -17,15 +15,19 @@ const NotAuthError = () => <p>Inicia sesión para ver este contenido.</p>;
 const UsersList = () => {
   const auth = useAppSelector(authSelector);
   const dispatch = useAppDispatch();
+  const { items: defaultItems, page, pages } = useAppSelector(usersSelector);
+  const [items, setItems] = useState<User[]>(defaultItems);
   useEffect(() => {
     const fetch = async () => {
       console.log('FETCHING...');
       let { data } = await axios.get('/api/users', { headers: { token: auth.user?.token } });
+      console.log('DATA', data);
       dispatch(setListData({ items: data }));
+      setItems(data);
     }
     fetch();
   }, [])
-  const { page, pages, items } = useAppSelector(usersSelector);
+
   console.log('PAGE', page, 'PAGES', pages, 'ITEMS', items);
   return <>
     <p>Hola</p>
@@ -38,5 +40,7 @@ const UsersList = () => {
     </ul>
   </>;
 }
+
+// <<<<<<< https://youtu.be/NZKUirTtxcg?t=309
 
 export default IndexPage;
